48caf776afc5a5c1dfb8aceab595c15ec7847f45
Goswamip548 <150900601+Goswamip548@users.noreply.github.com>
2023-11-20 16:45:09 +0100

    Update Make_header.sh

--- Make_header.sh
+++ Make_header.sh
@@ -1,1 +1,8 @@
 head -n +2 115_vep.maf | tail -n +2 > header.maf
+sed 's/TUMOR/TUMOR_67/g' 67adeno.maf > 67_adeno.maf
+sed 's/TUMOR/TUMOR_99/g' 99adeno.maf > 99_adeno.maf
+sed 's/TUMOR/TUMOR_115/g' 115adeno.maf > 115_adeno.maf
+sed 's/TUMOR/TUMOR_120/g' 120adeno.maf > 120_adeno.maf
+cat header.maf 67_adeno.maf 99_adeno.maf 115_adeno.maf 120_adeno.maf 127_adeno.maf 
+129_adeno.maf 130_adeno.maf 140_adeno.maf 152_adeno.maf 162_adeno.maf 168_adeno.maf 
+169_adeno.maf > adeno.maf
